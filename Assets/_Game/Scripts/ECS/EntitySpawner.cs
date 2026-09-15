47910a564d004a6f74bcc6ccf39678ce67666fcf
ac1dsunje <ar.cherba63programming@gmail.com>
2026-09-15 14:16:34 +0200

    refactoring

--- Assets/_Game/Scripts/ECS/EntitySpawner.cs
+++ Assets/_Game/Scripts/ECS/EntitySpawner.cs
@@ -10,22 +10,17 @@ public class EntitySpawner
 {
     public void SpawnPlayer(Vector2 position, Rigidbody2D rigidBody)
     {
-        W.NewEntity<Default>().Set(
-            new PositionComponent { Position = position },
-            new InputComponent { Direction = Vector2.zero },
-            new RigidBodyComponent { Body = rigidBody },
-            
-            new HealthComponent { Value = 5 },
-            new MaxHealthComponent { Value = 10 },
-            new RegenerationComponent { Value = 1},
-            
-            new MaxSpeedComponent { Value = 1 }
-        ).Set<PlayerControlledTag>();
+        CreateBaseEntity(position, rigidBody).Set<PlayerControlledTag>();
     }
     
     public void SpawnEnemy(Vector2 position, Rigidbody2D rigidBody)
     {
-        W.NewEntity<Default>().Set(
+        CreateBaseEntity(position, rigidBody).Set<AIControlledTag>();
+    }
+    
+    private World<GameWorld>.Entity CreateBaseEntity(Vector2 position, Rigidbody2D rigidBody)
+    {
+        return W.NewEntity<Default>().Set(
             new PositionComponent { Position = position },
             new InputComponent { Direction = Vector2.zero },
             new RigidBodyComponent { Body = rigidBody },
@@ -35,7 +30,7 @@ public class EntitySpawner
             new RegenerationComponent { Value = 1},
             
             new MaxSpeedComponent { Value = 1 }
-        ).Set<AIControlledTag>();
+        );
     }
 }
 }
