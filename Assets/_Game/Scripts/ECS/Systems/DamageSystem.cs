d5c7f9f7fef227be6b7f373d9378867887527adf
ac1dsunje <ar.cherba63programming@gmail.com>
2026-09-16 12:41:29 +0200

    renamings

--- Assets/_Game/Scripts/ECS/Systems/DamageSystem.cs
+++ Assets/_Game/Scripts/ECS/Systems/DamageSystem.cs
@@ -21,18 +21,18 @@ public struct DamageSystem : ISystem
             if (!e.Value.Target.TryUnpack<GameWorld>(out var target))
                 continue;
 
-            ref var healthComp = ref target.Ref<HealthComponent>();
-            ref var maxHealthComp = ref target.Ref<MaxHealthComponent>();
+            ref var health = ref target.Ref<HealthComponent>();
+            ref var maxHealth= ref target.Ref<MaxHealthComponent>();
             ref var extraLives = ref target.Ref<ExtraLivesComponent>();
             ref var resistance = ref target.Ref<DamageResistanceComponent>();
             
-            healthComp.Value -= e.Value.Damage * (1 - (resistance.Value - e.Value.IgnoreResistance) / 100f);
+            health.Value -= e.Value.Damage * (1 - (resistance.Value - e.Value.IgnoreResistance) / 100f);
             
-            if (healthComp.Value <= 0)
+            if (health.Value <= 0)
             {
                 if (extraLives.Value > 0)
                 {
-                    healthComp.Value = maxHealthComp.Value;
+                    health.Value = maxHealth.Value;
                     extraLives.Value--;
                 }
                 else
