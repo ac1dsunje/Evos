b6e0b23a5d9614d8f6a67cae0743b2cb4c9a72ec
ac1dsunje <ar.cherba63programming@gmail.com>
2026-09-18 19:53:03 +0200

    deleted stats init event

--- Assets/_Game/Scripts/ECS/Features/Config/StatsConfigComponent.cs
+++ Assets/_Game/Scripts/ECS/Features/Config/StatsConfigComponent.cs
@@ -1,20 +1,10 @@
 ﻿using _Game.Scripts.Configs;
-using _Game.Scripts.ECS.Features.Stats;
 using FFS.Libraries.StaticEcs;
 
 namespace _Game.Scripts.ECS.Features.Config
 {
-public struct StatsConfigComponent : IComponent
+public struct StatsConfigComponent : IComponent, ITrackableAdded
 {
     public StatsConfig Config;
-
-    public void OnAdd<TWorld>(World<TWorld>.Entity self) where TWorld : struct, IWorldType
-    {
-        W.SendEvent(new InitStatsEvent
-        {
-            Target = self.GID,
-            Config = Config
-        });
-    }
 }
 }
--- Assets/_Game/Scripts/ECS/Features/Stats/StatsInitSystem.cs
+++ Assets/_Game/Scripts/ECS/Features/Stats/StatsInitSystem.cs
@@ -2,6 +2,7 @@
 using _Game.Scripts.ECS.Features.Biomes;
 using _Game.Scripts.ECS.Features.Biomes.Breathing;
 using _Game.Scripts.ECS.Features.Biomes.Temperature;
+using _Game.Scripts.ECS.Features.Config;
 using _Game.Scripts.ECS.Features.Defense;
 using _Game.Scripts.ECS.Features.Endurance;
 using _Game.Scripts.ECS.Features.Health;
@@ -16,79 +17,69 @@ namespace _Game.Scripts.ECS.Features.Stats
 {
 public struct StatsInitSystem : ISystem
 {
-    private EventReceiver<GameWorld, InitStatsEvent> _receiver;
-
-    public void Init()
-    {
-        _receiver = W.RegisterEventReceiver<InitStatsEvent>();
-    }
-
     public void Update()
     {
-        foreach (var e in _receiver)
+        foreach (var entity in W.Query<AllAdded<StatsConfigComponent>>().Entities())
         {
-            if (!e.Value.Target.TryUnpack<GameWorld>(out var target))
-                continue;
+            ref readonly var config = ref entity.Read<StatsConfigComponent>().Config;
             
-            var config = e.Value.Config;
-            
-            target.Set(
+            entity.Set(
                 new MaxHealthComponent { Value = config.MaxHealth },
                 new HealthComponent { Value = config.MaxHealth }
                 );
             
-            target.Set(
+            entity.Set(
                 new MaxHungerComponent { Value = config.MaxHunger },
                 new HungerComponent { Value = config.MaxHunger }
                 );
             
-            target.Set(
+            entity.Set(
                 new MaxEnduranceComponent { Value = config.MaxEndurance },
                 new EnduranceComponent { Value = config.MaxEndurance }
                 );
             
-            target.Set(
+            entity.Set(
                 new ExtraLivesComponent { Value = config.ExtraLives },
                 new RegenerationComponent { Value = config.Regeneration },
                 new EnduranceRecoveryComponent { Value = config.EnduranceRecovery }
                 );
             
-            target.Set(
+            entity.Set(
                 new MaxSpeedComponent { Value = config.MaxSpeed },
                 new AccelerationComponent { Value = config.Acceleration },
                 new InertiaComponent { Value = config.Inertia }
                 );
             
-            target.Set(
+            entity.Set(
                 new DashRangeComponent { Value = config.DashRange },
                 new SprintMultiplierComponent { Value = config.SprintMultiplier }
             );
             
-            target.Set(
+            entity.Set(
                 new ColdResistanceComponent { Value = config.ColdResistance },
                 new HotResistanceComponent { Value = config.HotResistance }
             );
             
-            target.Set(
+            entity.Set(
                 new ReflectionComponent { Value = config.Reflection },
                 new ResistanceComponent { Value = config.Resistance }
             );
             
-            target.Set(
+            entity.Set(
                 new OxygenComponent { Value = config.OxygenRequirement },
                 new HydrogenComponent { Value = config.HydrogenRequirement }
             );
             
-            target.Set(new InfluenceComponent { Value = config.Influence });
+            entity.Set(new InfluenceComponent { Value = config.Influence });
             
-            target.Set(new PassAbilityComponent { Value = config.PassAbility });
+            entity.Set(new PassAbilityComponent { Value = config.PassAbility });
             
-            target.Set(
+            entity.Set(
                 new PhysicalDamageComponent { Value = config.PhysicalDamage },
                 new ResistanceIgnoreComponent { Value = config.IgnoreResistance }
             );
             
-            target.Set(new PickingRangeComponent { Value = config.PickingRange });
+            entity.Set(new PickingRangeComponent { Value = config.PickingRange });
         }
     }
 }
